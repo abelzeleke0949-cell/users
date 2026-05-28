2855339fff1a4a406b7be001ff93cc38fc19eaf9
abelzeleke0949-cell <abelzeleke0949@gmail.com>
2026-05-28 01:42:25 -0700

    update in darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
-import React, { useState, useEffect } from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import UserDetails from "../pages/UserDetails";
 
 
 function App() {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("darkMode") === "true";
-  }
-  );
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-    localStorage.setItem("darkMode", darkMode);
-  }, [darkMode]);
 
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100 dark:bg-gray-900"></div>
       <Routes>
         <Route
           path="/"
           element={
-            <Home darkMode={darkMode} setDarkMode={setDarkMode} />
+            <Home  />
           }
         />
         <Route
           path="/users/:id"
           element={
-            <UserDetails darkMode={darkMode} setDarkMode={setDarkMode} />
+            <UserDetails/>
           }
         />
       </Routes>
